89f0f2c6ecf7007276ab6a51445c4a7da819b92e
blackmase <1mason.davis@gmail.com>
2020-12-11 19:24:49 -1000

    what i have thus far

---
+++ STFUapp/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyAMjSS3yxZ3lExQlsEjT7rBzFoWbCI0XrQ",
+    authDomain: "stfu-9a779.firebaseapp.com",
+    projectId: "stfu-9a779",
+    storageBucket: "stfu-9a779.appspot.com",
+    messagingSenderId: "182490595662",
+    appId: "1:182490595662:web:66ec1f76f08ecd17c5de1f",
+    measurementId: "G-YBL9X9RJM4"
+};
